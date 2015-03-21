34fba1ad05a53d9acef20c0bf828643aa1a69c4a
SYNC Technologies <syncnologies@SYNCs-Mac-mini.local>
2015-03-21 11:37:48 +0530

    Initial Commit

---
+++ TestApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApplication
+//
+//  Created by SYNC Technologies on 21/03/15.
+//  Copyright (c) 2015 SYNC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApplication
+//
+//  Created by SYNC Technologies on 21/03/15.
+//  Copyright (c) 2015 SYNC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
